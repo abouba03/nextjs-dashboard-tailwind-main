53ec85baa7b22cf2701a6e5da53dfeae72c007e9
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-11-19 09:27:46 +0300

    render dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
-import Image from 'next/image';
-
 import Header from '../components/Header';
 import TopCards from '../components/TopCards';
 import BarChart from '../components/BarChart';
 import RecentOrders from '../components/RecentOrders';
+import LineChart from '@/components/LineChart';
+import PieChart from '@/components/PieChart';
+import ScatterChart from '@/components/ScatterChart';
 
 export default function Home() {
   return (
@@ -18,8 +19,13 @@ export default function Home() {
       <main className='bg-gray-100 min-h-screen'>
         <Header />
         <TopCards />
-        <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
-          <BarChart />
+        <div className='p-4 grid md:grid-cols-2 grid-cols-1 gap-4'>
+          <div className='flex flex-col gap-4'>
+            <BarChart />
+            <LineChart />
+            <PieChart />
+            <ScatterChart />
+          </div>
           <RecentOrders />
         </div>
       </main>
